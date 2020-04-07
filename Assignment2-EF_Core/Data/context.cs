bbb8b1facff1002db263f084449942669da0d2f3
Mikkel-KR <Mikkel.kora@gmail.com>
2020-04-07 15:45:08 +0200

    Lavet lidt ting

--- Assignment2-EF_Core/Data/context.cs
+++ Assignment2-EF_Core/Data/context.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using Assignment2_EF_Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment2_EF_Core.Data
 {
-    public class context
+    public class context : DbContext
     {
+        // Database Sets
+        public DbSet<Student> Students;
 
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            //ob.UseSqlServer();
+        }
     }
 }
