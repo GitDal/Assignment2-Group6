ea7101c9fcabe522614f0a6a47c228bbc556d3a3
Mikkel-KR <Mikkel.kora@gmail.com>
2020-04-07 16:43:13 +0200

    Begyndt på HelpRequestController

---
+++ Assignment2-ASP_NET/Controllers/HelpRequestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment2_EF_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2_ASP_NET.Controllers
+{
+    public class HelpRequestController : Controller
+    {
+        public IActionResult Overview()
+        {
+            return View();
+        }
+
+        public IActionResult All(int studentAuId)
+        {
+
+        }
+
+        public IActionResult All(string teacherAuId, int courseId)
+        {
+
+        }
+
+        public IActionResult Statistics()
+        {
+
+
+            return View();
+        }
+
+
+    }
+}
--- Assignment2-ASP_NET/Controllers/HomeController.cs
+++ Assignment2-ASP_NET/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Assignment2_ASP_NET.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
