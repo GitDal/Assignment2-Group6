64ab88997e75c7e8343ee79d0fa08669fa6ff915
GitDal <jeppedybdal@gmail.com>
2020-04-07 15:45:38 +0200

    Modeller

---
+++ Assignment2-EF_Core/Models/Exercise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Assignment2_EF_Core.Models
+{
+    public class Exercise
+    {
+        [Required]
+        public int Number { get; set; }
+        [Required]
+        public string Lecture { get; set; }
+        [Required]
+        public string HelpWhere { get; set; }
+    }
+}
