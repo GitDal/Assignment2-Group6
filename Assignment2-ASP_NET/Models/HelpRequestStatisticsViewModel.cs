30d45a017ea8188ca40116a76226eda60dc5f729
Mikkel-KR <Mikkel.kora@gmail.com>
2020-04-10 23:38:06 +0200

    Made Statistics yeet

---
+++ Assignment2-ASP_NET/Models/HelpRequestStatisticsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Assignment2_ASP_NET.Database.Models;
+
+namespace Assignment2_ASP_NET.Models
+{
+    public class HelpRequestStatisticsViewModel
+    {
+        public HelpRequestStatisticsViewModel() { }
+
+        public List<HelpRequestCourseStatistics> CourseStatisticsList { get; set; }
+    }
+
+    public class HelpRequestCourseStatistics
+    {
+        public HelpRequestCourseStatistics()
+        { }
+
+        public Course Course { get; set; }
+        public int ExerciseAmount { get; set; }
+        public int AssignmentAmount { get; set; }
+        public int OpenAmount { get; set; }
+    }
+}
